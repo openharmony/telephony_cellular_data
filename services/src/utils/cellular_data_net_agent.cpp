265e7a6b10a411c23b4f735f65f7fb9c2f6090a2
dingxiaochen <dingxiaochen@huawei.com>
2023-01-16 11:40:46 +0800

    fix cellular data not enable.
    Signed-off-by: dingxiaochen <dingxiaochen@huawei.com>

--- services/src/utils/cellular_data_net_agent.cpp
+++ services/src/utils/cellular_data_net_agent.cpp
@@ -61,7 +61,7 @@ bool CellularDataNetAgent::RegisterNetSupplier(const int32_t slotId)
         uint32_t supplierId = 0;
         int32_t result = netManager->RegisterNetSupplier(
             NetBearType::BEARER_CELLULAR, std::string(IDENT_PREFIX) + std::to_string(simId), netCap, supplierId);
-        if (result == 0) {
+        if (result == NETMANAGER_SUCCESS) {
             TELEPHONY_LOGI("Register network successful, supplierId[%{public}d]", supplierId);
             flag = true;
             netSupplier.supplierId = supplierId;
