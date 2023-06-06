daba048646f7f48f48d48caf0dc7e1beab9543ba
liuxiyao223 <liuxiyao223@huawei.com>
2023-06-06 14:30:16 +0800

    cellular_data inner api优化 Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- interfaces/innerkits/cellular_data_client.h
+++ interfaces/innerkits/cellular_data_client.h
@@ -30,19 +30,113 @@ class CellularDataClient : public DelayedRefSingleton<CellularDataClient> {
     DECLARE_DELAYED_REF_SINGLETON(CellularDataClient);
 
 public:
+    /**
+     * @brief Whether cellular data service is connected.
+     *
+     * @return Return true on connected, false on not connected.
+     */
     bool IsConnect() const;
+
+    /**
+     * @brief Whether to enable cellular data user switch
+     *
+     * @param enable Enable or not.
+     * @return Return 84082688 invalid parameter, 1 data enable success, 0 enable fail.
+     */
     int32_t EnableCellularData(bool enable);
+
+    /**
+     * @brief Whether the cellular data user switch is enabled
+     *
+     * @param dataEnabled Indicates the result of data enabled status.
+     * @return Returns error code.
+     */
     int32_t IsCellularDataEnabled(bool &dataEnabled);
+
+    /**
+     * @brief Cellular data connection status
+     *
+     * @return Returns data connection status defined in DataConnectionStatus.
+     */
     int32_t GetCellularDataState();
+
+    /**
+     * @brief Whether roaming is allowed
+     *
+     * @param slotId Indicates card slot identification
+     * @param dataRoamingEnabled Indicates the result of data roaming enabled status.
+     * @return Returns error code.
+     */
     int32_t IsCellularDataRoamingEnabled(int32_t slotId, bool &dataRoamingEnabled);
+
+    /**
+     * @brief Whether roaming switches are allowed
+     *
+     * @param slotId card slot identification
+     * @param enable Whether roaming switches are allowed
+     * @return Returns 0 on failure, 1 on failure. 84082688 invalid parameter
+     */
     int32_t EnableCellularDataRoaming(int32_t slotId, bool enable);
+
+    /**
+     * @brief Get the slotId that uses the data traffic by default
+     *
+     * @return Returns the default settings data card, -1 error code
+     */
     int32_t GetDefaultCellularDataSlotId();
+
+    /**
+     * Get the slotId that uses the data traffic by default
+     *
+     * @param simId Returns default settings data sim id
+     * @return Returns 0 on success, others on failure.
+     */
     int32_t GetDefaultCellularDataSimId(int32_t &simId);
+
+    /**
+     * @brief Set the slotId that uses the data traffic by default
+     *
+     * @param slotId card slot identification
+     * @return 1 set success, 0 set fail, 84082688 invalid parameter
+     */
     int32_t SetDefaultCellularDataSlotId(int32_t slotId);
+
+    /**
+     * @brief Get data packet type
+     *
+     * @return Returns cell data flow type defined in CellDataFlowType.
+     */
     int32_t GetCellularDataFlowType();
+
+    /**
+     * @brief Whether cellular data has internet capability.
+     *
+     * @param slotId Card slot identification.
+     * @param cid Context identification.
+     * @return Return 1 if has, 0 if hasn't.
+     */
     int32_t HasInternetCapability(int32_t slotId, int32_t cid);
+
+    /**
+     * @brief Clear cellular data connections.
+     *
+     * @param slotId Card slot identification.
+     * @return 1 set success, 0 set fail, 84082688 invalid parameter
+     */
     int32_t ClearCellularDataConnections(int32_t slotId);
+
+    /**
+     * @brief Get cellular data proxy.
+     *
+     * @return Cellular data service.
+     */
     sptr<ICellularDataManager> GetProxy();
+
+    /**
+     * @brief Update the slotId that uses the data traffic by default
+     *
+     * @return Returns the default settings data card, -1 error code
+     */
     int32_t UpdateDefaultCellularDataSlotId();
 
 private:
