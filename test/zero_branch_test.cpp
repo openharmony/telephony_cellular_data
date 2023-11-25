4d6b08232e680d559af6f4c9315b57b41790882a
wulimeimei <hanjing5@huawei.com>
2023-11-25 15:02:41 +0800

    Add TDD
    Signed-off-by: wulimeimei <hanjing5@huawei.com>

--- test/zero_branch_test.cpp
+++ test/zero_branch_test.cpp
@@ -367,6 +367,7 @@ HWTEST_F(BranchTest, Telephony_CellularDataConnectionManager_001, Function | Med
     DataConnectionManager con { runner, 0 };
     con.connectionMonitor_ = nullptr;
     con.ccmDefaultState_ = nullptr;
+    con.stateMachineEventHandler_ = nullptr;
     std::shared_ptr<CellularDataStateMachine> stateMachine = nullptr;
     con.RemoveConnectionStateMachine(stateMachine);
     ASSERT_TRUE(con.GetActiveConnectionByCid(0) == nullptr);
@@ -542,6 +543,12 @@ HWTEST_F(BranchTest, Telephony_ApnHolder_003, Function | MediumTest | Level3)
     newApnItem = ApnItem::MakeDefaultApn(DATA_CONTEXT_ROLE_DEFAULT);
     oldApnItem = ApnItem::MakeDefaultApn(DATA_CONTEXT_ROLE_DEFAULT);
     ASSERT_TRUE(apnHolder->IsSameApnItem(newApnItem, oldApnItem, true));
+    oldApnItem->CanDealWithType(DATA_CONTEXT_ROLE_DEFAULT);
+    oldApnItem->GetApnTypes();
+    PdpProfile apnBean;
+    oldApnItem->MakeApn(apnBean);
+    oldApnItem->MarkBadApn(false);
+    ASSERT_FALSE(oldApnItem->IsBadApn());
 }
 
 /**
@@ -735,6 +742,12 @@ HWTEST_F(BranchTest, ApnManager_Test_01, Function | MediumTest | Level3)
     std::string operatorNumeric = "46003";
     apnManager->GetCTOperator(0, operatorNumeric);
     EXPECT_EQ(operatorNumeric, "46003");
+    apnManager->GetApnHolder(DATA_CONTEXT_ROLE_DEFAULT);
+    apnManager->FindApnNameByApnId(1);
+    std::shared_ptr<StateMachineTest> machine = std::make_shared<StateMachineTest>();
+    std::shared_ptr<CellularDataStateMachine> cellularMachine = machine->CreateCellularDataConnect(0);
+    cellularMachine->Init();
+    ASSERT_TRUE(apnManager->IsDataConnectionNotUsed(cellularMachine));
     auto helper = CellularDataRdbHelper::GetInstance();
     std::shared_ptr<DataShare::DataShareResultSet> result = nullptr;
     std::vector<PdpProfile> apnVec;
