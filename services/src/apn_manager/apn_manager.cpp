2e90e53b8599e819c34eb143830c025f2ceccf2f
汪少俊 <newstriving@126.com>
2024-07-24 11:14:46 +0800

    remove supl apn type
    Signed-off-by: 汪少俊 <newstriving@126.com>

--- services/src/apn_manager/apn_manager.cpp
+++ services/src/apn_manager/apn_manager.cpp
@@ -252,7 +252,7 @@ int32_t ApnManager::CreateAllApnItemByDatabase(int32_t slotId)
 {
     int32_t count = 0;
     if (TELEPHONY_EXT_WRAPPER.createAllApnItemExt_) {
-        sptr<ApnItem> extraApnItem = ApnItem::MakeDefaultApn("default,supl");
+        sptr<ApnItem> extraApnItem = ApnItem::MakeDefaultApn("default");
         if (TELEPHONY_EXT_WRAPPER.createAllApnItemExt_(slotId, extraApnItem)) {
             allApnItem_.clear();
             allApnItem_.push_back(extraApnItem);
