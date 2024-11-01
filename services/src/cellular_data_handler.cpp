884282462b61af146c6782ae80705c4a7fa8f9ba
zhangjie <2756669622@qq.com>
2024-11-01 00:13:41 +0000

    update services/src/cellular_data_handler.cpp.
    Signed-off-by: zhangjie <2756669622@qq.com>

--- services/src/cellular_data_handler.cpp
+++ services/src/cellular_data_handler.cpp
@@ -2392,11 +2392,7 @@ void CellularDataHandler::ReleaseCellularDataConnection()
         TELEPHONY_LOGI("system using, can not release");
         return;
     }
-    if (apnHolder->GetApnState() == ApnProfileState::PROFILE_STATE_IDLE) {
-        TELEPHONY_LOGI("apn state idle, can not release");
-        return;
-    }
-    apnHolder->ReleaseDataConnection();
+    ClearConnection(anpHolder, DisConnectionReason::REASON_CLEAR_CONNECTION);
 }
 
 bool CellularDataHandler::UpdateNetworkInfo()
