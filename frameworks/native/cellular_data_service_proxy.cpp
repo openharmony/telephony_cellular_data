3eba288e1eb84dd1e9ff5571d6f31a27a82ea51b
zhangwuling1 <zhangsheng81@huawei.com>
2025-03-21 08:50:49 +0000

    update frameworks/native/cellular_data_service_proxy.cpp.
    Signed-off-by: zhangwuling1 <zhangsheng81@huawei.com>

--- frameworks/native/cellular_data_service_proxy.cpp
+++ frameworks/native/cellular_data_service_proxy.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <limits>
+
 #include "cellular_data_service_proxy.h"
 
 #include "iremote_object.h"
