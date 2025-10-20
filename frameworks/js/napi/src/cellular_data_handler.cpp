9412000784f3b4ac63c5d046a8891348011e4dc2
qq_36620091 <wangcheng214@huawei.com>
2025-10-20 21:17:25 +0800

    update: 更新文件 cellular_data_handler.cpp
    Signed-off-by: qq_36620091 <wangcheng214@huawei.com>

--- frameworks/js/napi/src/cellular_data_handler.cpp
+++ frameworks/js/napi/src/cellular_data_handler.cpp
@@ -2029,7 +2029,7 @@ bool CellularDataHandler::GetEsmFlagFromOpCfg()
 void CellularDataHandler::SetRilAttachApn()
 {
     if (!IsSimStateReadyOrLoaded()) {
-        TELEPHONY_LOGE("Slot%{punlic}d:sim not ready",slotId_);
+        TELEPHONY_LOGE("Slot%{punlic}d: sim not ready", slotId_);
         return;
     }
     DataProfile dataProfile;
