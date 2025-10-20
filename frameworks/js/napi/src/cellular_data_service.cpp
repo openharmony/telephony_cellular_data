9da55494973850d8048ee07821d7c6e328105bd6
wmyao_mm <wangmengyao17@huawei.com>
2025-10-20 16:58:02 +0800

    增加接口校验
    Signed-off-by: wmyao_mm <wangmengyao17@huawei.com>

--- frameworks/js/napi/src/cellular_data_service.cpp
+++ frameworks/js/napi/src/cellular_data_service.cpp
@@ -219,6 +219,10 @@ int32_t CellularDataService::EnableIntelligenceSwitch(bool enable)
 
 int32_t CellularDataService::GetCellularDataState(int32_t &state)
 {
+    if (!TelephonyPermission::CheckPermission(Permission::GET_NETWORK_INFO))
+    {
+        return TELEPHONY_ERR_PERMISSION_ERR;
+    }
     int32_t slotId;
     CellularDataService::GetDefaultCellularDataSlotId(slotId);
 #ifdef OHOS_BUILD_ENABLE_TELEPHONY_EXT
@@ -554,6 +558,10 @@ int32_t CellularDataService::SetDefaultCellularDataSlotId(const int32_t slotId)
 
 int32_t CellularDataService::GetCellularDataFlowType(int32_t &type)
 {
+    if (!TelephonyPermission::CheckPermission(Permission::GET_NETWORK_INFO))
+    {
+        return TELEPHONY_ERR_PERMISSION_ERR;
+    }
     int32_t slotId;
     CellularDataService::GetDefaultCellularDataSlotId(slotId);
     std::shared_ptr<CellularDataController> cellularDataController = GetCellularDataController(slotId);
