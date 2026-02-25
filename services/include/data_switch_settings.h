21db1f18bbf923269f7d693e9874a964f4349097
qq_36620091 <wangcheng214@huawei.com>
2026-02-25 17:27:27 +0800

    update: 更新文件 data_switch_settings.h
    Signed-off-by: qq_36620091 <wangcheng214@huawei.com>

--- services/include/data_switch_settings.h
+++ services/include/data_switch_settings.h
@@ -55,7 +55,7 @@ private:
     bool intelliSwitchOn_ = false;
     const int32_t slotId_;
     int32_t lastQryRet_ = -1;
-    std::mutex userDataOnMutex_;
+    mutable std::mutex userDataOnMutex_;
 };
 } // namespace Telephony
 } // namespace OHOS
