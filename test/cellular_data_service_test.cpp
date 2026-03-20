730225b8b5a5126c706bd22b9fa613e78a5acde9
OoSpence <yuanshengsi@h-partners.com>
2026-03-20 18:33:17 +0800

    fix dorecovery exec CGCATT fail
    Signed-off-by: OoSpence <yuanshengsi@h-partners.com>

--- test/cellular_data_service_test.cpp
+++ test/cellular_data_service_test.cpp
@@ -175,10 +175,14 @@ HWTEST_F(CellularDataServiceTest, DataConnectionMonitor_OnStallDetectionTimer_00
 HWTEST_F(CellularDataServiceTest, DataConnectionMonitor_OnStallDetectionTimer_002, TestSize.Level1)
 {
     std::shared_ptr<DataConnectionMonitor> dataConnectionMonitor = std::make_shared<DataConnectionMonitor>(0);
-    dataConnectionMonitor->noRecvPackets_ = 10;
-    dataConnectionMonitor->dataRecoveryState_ = RecoveryState::STATE_RADIO_STATUS_RESTART;
+    dataConnectionMonitor->noRecvPackets_ = 0;
     dataConnectionMonitor->stallDetectionEnabled_ = true;
+    dataConnectionMonitor->dataRecoveryState_ = RecoveryState::STATE_REQUEST_CONTEXT_LIST;
     dataConnectionMonitor->OnStallDetectionTimer();
+    ASSERT_EQ(dataConnectionMonitor->dataRecoveryState_, RecoveryState::STATE_REQUEST_CONTEXT_LIST);
+    dataConnectionMonitor->dataRecoveryState_ = RecoveryState::STATE_RADIO_STATUS_RESTART;
+    dataConnectionMonitor->OnStallDetectionTimer();
+    dataConnectionMonitor->dataRecoveryState_ = RecoveryState::STATE_RADIO_STATUS_RESTART;
     dataConnectionMonitor->HandleRecovery();
     ASSERT_EQ(dataConnectionMonitor->dataRecoveryState_, RecoveryState::STATE_REQUEST_CONTEXT_LIST);
 }
