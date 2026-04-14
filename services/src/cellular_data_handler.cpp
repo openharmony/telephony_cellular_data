b2134b364f5cc9ee6d34f00d22028ef98865bb8b
qq_36620091 <wangcheng214@huawei.com>
2026-04-14 21:17:48 +0800

    update: 更新文件 cellular_data_handler.cpp
    Signed-off-by: qq_36620091 <wangcheng214@huawei.com>

--- services/src/cellular_data_handler.cpp
+++ services/src/cellular_data_handler.cpp
@@ -2542,11 +2542,11 @@ void CellularDataHandler::HandleDBSettingEnableChanged(const AppExecFwk::InnerEv
         return;
     }
     bool dataEnabled = true;
-    if (SIM_SLOT_COUNT == 1) {
-        dataEnabled = dataSwitchSettings_->IsUserDataOn();
-    } else {
-        dataSwitchSettings_->QueryUserDataStatus(dataEnabled);
-    }
+#ifdef FEATURE_SINGLE_CARD
+    dataEnabled = dataSwitchSettings_->IsUserDataOn();
+#else
+    dataSwitchSettings_->QueryUserDataStatus(dataEnabled);
+#endif
     CoreManagerInner &coreInner = CoreManagerInner::GetInstance();
     const int32_t defSlotId = coreInner.GetDefaultCellularDataSlotId();
     std::string dataPolicy = system::GetParameter(PERSIST_EDM_MOBILE_DATA_POLICY, "");
