80acfa2a8d7f315d4b669570279ca527c15c5648
lsyhh <331993722@qq.com>
2026-05-12 09:38:49 +0800

    update: 更新文件 apn_holder.cpp
    Signed-off-by: lsyhh <331993722@qq.com>

--- services/src/apn_manager/apn_holder.cpp
+++ services/src/apn_manager/apn_holder.cpp
@@ -19,11 +19,11 @@
 
 namespace OHOS {
 namespace Telephony {
-namespace { 
-     constexpr int32_t SYSTEM_UID = 1e4; 
-     constexpr int32_t NETMANAGER_UID = 1099; 
-     std::shared_mutex reqUidsMutex_; 
-     std::mutex netRequestMutex_; 
+namespace {
+    constexpr int32_t SYSTEM_UID = 1e4;
+    constexpr int32_t NETMANAGER_UID = 1099;
+    std::shared_mutex reqUidsMutex_;
+    std::mutex netRequestMutex_;
 }
 const std::map<std::string, int32_t> ApnHolder::apnTypeDataProfileMap_ {
     {DATA_CONTEXT_ROLE_DEFAULT, DATA_PROFILE_DEFAULT},
