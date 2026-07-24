75233f122f942800169c941af36576d9d64bfc6e
Zhang_Wuling <zhangsheng81@huawei.com>
2026-07-24 10:53:48 +0800

    fix rizhi
    Signed-off-by: Zhang_Wuling <zhangsheng81@huawei.com>

--- test/zero_branch_test.cpp
+++ test/zero_branch_test.cpp
@@ -2553,6 +2553,13 @@ HWTEST_F(BranchTest, Telephony_CellularDataConnectionManager_003, Function | Med
     ASSERT_TRUE(ccmDefaultState.StateProcess(event));
     event = AppExecFwk::InnerEvent::Get(RadioEvent::RADIO_NETWORKSLICE_EHPLMN_RPT);
     ASSERT_TRUE(ccmDefaultState.StateProcess(event));
+    system::SetParameter("persist.netmgr_ext.networkslice", "true");
+    event = AppExecFwk::InnerEvent::Get(RadioEvent::RADIO_NETWORKSLICE_URSP_RPT);
+    ASSERT_TRUE(ccmDefaultState.StateProcess(event));
+    event = AppExecFwk::InnerEvent::Get(RadioEvent::RADIO_NETWORKSLICE_ALLOWEDNSSAI_RPT);
+    ASSERT_TRUE(ccmDefaultState.StateProcess(event));
+    event = AppExecFwk::InnerEvent::Get(RadioEvent::RADIO_NETWORKSLICE_EHPLMN_RPT);
+    ASSERT_TRUE(ccmDefaultState.StateProcess(event));
     event = AppExecFwk::InnerEvent::Get(0);
     ASSERT_FALSE(ccmDefaultState.StateProcess(event));
     ccmDefaultState.RadioDataCallListChanged(event);
